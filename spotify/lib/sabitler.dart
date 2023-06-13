1e489637d3fb15aa9c47c9efc7db8ea41b154fa4
Burhan Üçcan <burhan.uccann@gmail.com>
2023-06-13 14:26:31 +0300

    refresh

--- spotify/lib/sabitler.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
-
-class Sabitler {
-  static final boldFontStyle =
-      GoogleFonts.nunito(fontSize: 10, fontWeight: FontWeight.bold);
-  static final liteFontStyle =
-      GoogleFonts.nunito(fontSize: 13, fontWeight: FontWeight.normal);
-}
